2df3d5d4d4ee5ab7e5b41f167a184b42da3fa196
Ilya Sidorchik <ilya@sidorchik.ru>
2021-07-24 18:05:23 +0300

    Solve fol all cases

--- src/Calc/Calc.js
+++ src/Calc/Calc.js
@@ -1,36 +1,16 @@
-import algebra, { Equation, Fraction } from "algebra.js";
+import { calcGradePointAverage } from "./helpers";
 
 const Calc = () => {
   const fivePercent = 57.8;
   const fourPercent = 42.2;
 
-  var expr1 = algebra.parse(`${fivePercent}/100*(x+y)-x`);
-  var expr2 = algebra.parse(`${fourPercent}/100*(x+y)-y`);
-  var quation = new Equation(expr1, expr2);
-  var x = quation.solveFor("x");
+  const gradePointAverage = calcGradePointAverage(fivePercent, fourPercent);
 
-  const AVERAGE_EQUATION_PART_UP = "5x+4y";
-  const AVERAGE_EQUATION_PART_DOWN = "x+y";
-
-  const averageEquationPartUp = algebra.parse(AVERAGE_EQUATION_PART_UP);
-  const res1 = averageEquationPartUp.eval({ x });
-
-  const averageEquationPartDown = algebra.parse(AVERAGE_EQUATION_PART_DOWN);
-  const res2 = averageEquationPartDown.eval({ x });
-
-  const res1res = Number(res1.eval({ y: 1 }).multiply(211));
-  const res2res = Number(res2.eval({ y: 1 }).multiply(211));
-  const result = Number(new Fraction(res1res, res2res));
-
-  //   console.log(res1res.toString());
-  //   console.log(res2res.toString());
-  console.log("————");
-  console.log(result.toString());
-  //   console.log(downPart.toString());
+  console.log(gradePointAverage);
 
   return (
     <div>
-      <div>{result}</div>
+      <div>{gradePointAverage}</div>
     </div>
   );
 };
---
+++ src/Calc/helpers/calcGradePointAverage.js
@@ -0,0 +1,24 @@
+import algebra, { Equation } from "algebra.js";
+
+const AVERAGE_EQUATION_PART_UP = "5x+4y";
+const AVERAGE_EQUATION_PART_DOWN = "x+y";
+
+export const calcGradePointAverage = (fivePercent, fourPercent) => {
+  const expr1 = algebra.parse(`${fivePercent}/100*(x+y)-x`);
+  const expr2 = algebra.parse(`${fourPercent}/100*(x+y)-y`);
+  const quation = new Equation(expr1, expr2);
+  const x = quation.solveFor("x");
+
+  const averageEquationPartUp = algebra.parse(AVERAGE_EQUATION_PART_UP);
+  const partUp = averageEquationPartUp.eval({ x });
+  const averageEquationPartDown = algebra.parse(AVERAGE_EQUATION_PART_DOWN);
+  const partDown = averageEquationPartDown.eval({ x });
+
+  // eslint-disable-next-line no-eval
+  const partUpAnswer = eval(partUp.eval({ y: 1 }).toString());
+  // eslint-disable-next-line no-eval
+  const partDownAnswer = eval(partDown.eval({ y: 1 }).toString());
+  const result = (partUpAnswer / partDownAnswer).toFixed(2);
+
+  return result;
+};
